59be3354498a10c66cfe5ed18963f0c2f9abe107
Fajarshodiq1 <fajarshodiq1220@gmail.com>
2026-08-27 11:49:22 +0700

    resolve pointer highlight

--- components/ui/pointer-highlight.tsx
+++ components/ui/pointer-highlight.tsx
@@ -115,7 +115,7 @@ export function PointerHighlight({
   );
 }
 
-const Pointer = ({ ...props }: React.ReactSVGProps<SVGSVGElement>) => {
+const Pointer = ({ ...props }: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
       stroke="currentColor"
@@ -129,7 +129,7 @@ const Pointer = ({ ...props }: React.ReactSVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"></path>
+      <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z" />
     </svg>
   );
 };
